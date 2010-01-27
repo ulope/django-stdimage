81f632da42180737685865ca53cb224bbb45e16f
Ulrich Petri <u.petri@diefirma.de>
2010-01-27 18:28:58 +0100

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='django-stdimage',
+      version='0.11',
+      description='Django Standardized Image Field',
+      author='Marc Garcia',
+      author_email='',
+      url='http://github.com/ulope/django-stdimage',
+      download_url='git://github.com/ulope/django-stdimage.git',
+      packages=find_packages(),
+      license='MIT',
+      keywords='django image thumbnail',
+     )
